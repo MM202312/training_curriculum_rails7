a1007e7b8cb22b6c15017c2b2f76001d4ff7613d
MM202312 <152856541+MM202312@users.noreply.github.com>
2023-12-21 11:06:07 +0900

    Revert "Second training"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,18 +8,18 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-   Plan.create(plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
-    @wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday => @wdays[(@todays_date + x).wday], :plans => today_plans}
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
